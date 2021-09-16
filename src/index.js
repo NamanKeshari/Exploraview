988c8ce9244108d750a731486a14a76e42b83920
NamanPepcoding <89187802+NamanPepcoding@users.noreply.github.com>
2021-09-16 17:25:14 +0530

    imit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { StrictMode } from "react";
+import { render } from "react-dom";
+import App from "./App";
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
